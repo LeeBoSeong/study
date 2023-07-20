bb21a87c97641dca5585e78366b10e93928a1456
LeeBoSeong <ll0913@naver.com>
2023-07-20 18:15:34 +0900

    study

--- src/study01/lotto3.java
+++ src/study01/lotto3.java
@@ -1,24 +1,24 @@
 package study01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-public class lotto2 {
+public class lotto3 {
 	public static void main(String[] args) {
-		//indexOf를 이용하여 중복 검사(indexOf는 배열에선 사용 불가)
+		//contains를 이용한 중복 검사 배열은 안됌
 		Random random = new Random();
 		List<Integer> lNum = new ArrayList<Integer>();
-		
 		for(int i = 0; i < 6; i++) {
 			int r = random.nextInt(6)+1;
-			if(lNum.indexOf(r) == -1) {
+			if(!lNum.contains(r)) {
 				lNum.add(r);
 			}else {
-				--i;
+				i--;
 			}
 		}
-		for(int i = 0; i < lNum.size();i++) {
+		for(int i = 0; i < lNum.size(); i++) {
 			System.out.println(lNum.get(i));
 		}
 	}
